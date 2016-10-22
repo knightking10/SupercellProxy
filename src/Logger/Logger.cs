4902e059e2b7e29b63f5e82f37ea9aae64c28849
Lasertrap00 <lasertrap00@gmail.com>
2016-10-22 10:29:32 +0200

    Fixed Crash if Logs Directory is not existing

--- src/Logger/Logger.cs
+++ src/Logger/Logger.cs
@@ -59,6 +59,11 @@ namespace SupercellProxy
 
             // Log line to file
             string path = Environment.CurrentDirectory + @"\\Logs\\" + DateTime.UtcNow.ToLocalTime().ToString("dd-MM-yyyy") + ".log";
+
+            // Make sure the Logs Directory exists to prevent crash.
+            if (!Directory.Exists(Environment.CurrentDirectory + @"\\Logs\\"))
+                Directory.CreateDirectory(Environment.CurrentDirectory + @"\\Logs\\");
+
             using (FileStream fs = new FileStream(path, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
             {
                 using (StreamWriter StreamWriter = new StreamWriter(fs))
